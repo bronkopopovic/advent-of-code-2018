05cbe34aa56d9c6688d8a0298fcefc7e9a9632f2
roman <benutzer@romanMac.local>
2018-12-03 13:48:45 +0100

    day1.1

---
+++ day1_1/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut inp = File::open("src/input.txt").expect("File not found!");
+
+    let mut content = String::new();
+
+    inp.read_to_string(&mut content).expect("something went wrong reading the file");
+
+    let operations = content.split("\n");
+
+    let mut start = 0;
+
+    for i in operations {
+        let number: i32 = i.parse().expect("not a number");
+        start += number;
+    }
+
+    println!("{}", start);
+}
